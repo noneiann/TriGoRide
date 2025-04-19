31a73ccd94a382f057cb56bf2fcd5c27ad4bffcb
Zaimoo <reycezartigley@gmail.com>
2025-04-19 12:08:40 +0800

    Finished book-a-ride functionality with save to db.

--- lib/ui/screens/passenger_side/book_ride.dart
+++ lib/ui/screens/passenger_side/book_ride.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'location_picker_screen.dart';
+import '../location_picker_screen.dart';
 
 class BookRideScreen extends StatefulWidget {
   @override
